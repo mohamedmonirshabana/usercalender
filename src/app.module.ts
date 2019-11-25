87efe7beb07c2f0d2aff241437a78b30b6ad1a18
medo <mohamed.monir.shabana@gmail.com>
2019-11-25 16:34:41 +0200

    Fix error on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/userCalender')],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () =>{
+        return {
+          uri: 'mongodb://localhost:27017/usercalender',
+          useFindAndModify: false,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+        }
+      }
+    }), 
+    UsersModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
