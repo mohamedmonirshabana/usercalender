7471b635c2dd042748a3917d162d9dcfd478e1c4
medo <mohamed.monir.shabana@gmail.com>
2019-11-26 12:28:46 +0200

    Add Bcrypt and encrypt password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Body } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { CreateuserDto } from "./user.dto";
 
 @Controller('users')
 export class UsersController{
     constructor(private readonly userService: UsersService){}
 
+    @Post('register')
+    async adduser(
+        @Body() createuserDto: CreateuserDto){
+        console.log(createuserDto);
+        const result = await this.userService.Createuser(createuserDto);
+        //return {id: result};
+    }
+    
     @Get()
     getuser(){
         return this.userService.getuser();
     }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,16 +3,24 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserModel } from './users.model';
 import { CreateuserDto } from './user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService{
 
     constructor(@InjectModel('user') private readonly userModel: Model<UserModel> ){}
 
-    Createuser( createuserDto : CreateuserDto ): Promise<UserModel>{
+    async Createuser( createuserDto : CreateuserDto ){
+
+        const salt = await bcrypt.genSalt(10);
+        const pass = createuserDto.password;
+        const newpass  = await bcrypt.hash(pass, salt);
+        
+        createuserDto.password = newpass;
         const newuser = new this.userModel(createuserDto);
-        newuser.save();
-        return newuser;
+        await newuser.save();
+        return newuser.id;
+        //return newuser;
     }
     
     getuser():string{
